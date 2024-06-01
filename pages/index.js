af71e2fa63e2af08624a9a0a6f6033c25ba98eb4
Ulrica1620 <123502584+Ulrica1620@users.noreply.github.com>
2024-05-31 20:48:33 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,21 @@ import Footer from '@components/Footer'
 
 export default function Home() {
   return (
-    <div className="container">
-      <iframe
+  <div className="container">
+      <Head>
+        <title>Next.js Starter!</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <Header title="Welcome to my app!" />
+        <iframe
         src="https://30days.streamlit.app?embed=true"
         style="height: 450px; width: 100%;"
       ></iframe>
+      </main>
+
+      <Footer />
     </div>
   
   )
